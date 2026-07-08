98f998c15329e335194dea629b6bf9c0f86adce7
horiba807 <145101078+horiba807@users.noreply.github.com>
2026-07-08 10:09:22 +0900

    vite.config.js を更新

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
     // GitHubのリポジトリ名
     base: '/AnalyWallet/',
+});
+
+build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),       // トップページ
+                login: resolve(__dirname, 'login/index.html'), // ログインページ
+                about: resolve(__dirname, 'about/index.html'), 
+            },
+        },
+    },
 });
